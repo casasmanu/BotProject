f5cce72c8a598a0860e310089b17365a1c7af156
Chancha <manumcasasgmail.com>
2023-08-19 23:09:56 -0300

    eliminate duplicates

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import json
 from drvWebScraping import btc_scraping
 import schedule
 import time
-from drvTelegram import bot_send_msg,getNewUsers
+from drvTelegram import bot_send_msg, getNewUsers
+import numpy as np
 
-usd_prize=0
-arrUsers=[]
+usd_prize = 0
+arrUsers = []
 
 
-#update destinatary list
+# update destinatary list
 def updateDestinataryList():
     global arrUsers
     global jsonUsers
-    arrTempUsers=[]
-    #read the json file and update the variable
-    users_file= open('destinatorsList.txt','r+')
+    arrTempUsers = []
+    # read the json file and update the variable
+    users_file = open('destinatorsList.txt', 'r+',encoding='utf-8')
     users_lines = users_file.readlines()
     users_file.close()
     #################################################
@@ -24,44 +25,44 @@ def updateDestinataryList():
         tupla = text.split(' ')
         arrTempUsers.append(tupla)
 
-    
-    #######then read the requests for new users#######
-    arrNewTelegramUsers=getNewUsers(BOT_TOKEN)
+    ####### then read the requests for new users#######
+    arrNewTelegramUsers = getNewUsers(BOT_TOKEN)
 
-    
+    ids = [row[1] for row in arrTempUsers]
     ######### append - unify both lists ##############
-    ids=[row[1] for row in arrTempUsers]
     for newuser in arrNewTelegramUsers:
         if newuser[1] not in ids:
             arrTempUsers.append(newuser)
 
+    filterData = np.array(arrTempUsers)
     # now, with both lists in the same one, save it
     # in the txt and then replace the variable
-
+    filterData=np.unique(filterData,axis=0)
     ############ WRITE VARIABLE IN THE TXT ########
-    users_file = open('destinatorsList.txt', 'w')
-    for line in arrTempUsers:
+    users_file = open('destinatorsList.txt', 'w',encoding='utf-8')
+    for line in filterData:
         users_file.write(" ".join(line) + "\n")
     users_file.close()
-    #replace the global variable with the temporal one
-    if len(arrTempUsers)!=len(arrUsers):
-        arrUsers=arrTempUsers
+    # replace the global variable with the temporal one
+    if len(filterData) != len(arrUsers):
+        arrUsers = filterData
 
 
 def checkDolar():
     global usd_prize
     global arrUsers
-    act_prize=btc_scraping()
-    if act_prize != usd_prize :
-        usd_prize=act_prize
-        #update dolar for every registered user
+    act_prize = btc_scraping()
+    if act_prize != usd_prize:
+        usd_prize = act_prize
+        # update dolar for every registered user
         for user in arrUsers:
-            bot_send_msg(BOT_TOKEN,user[1],str(usd_prize))
+            bot_send_msg(BOT_TOKEN, user[1], str(usd_prize))
+
 
 # we are reading the token from the environmental variables of the OS, load initial settings
-file= open('settings.json')
+file = open('settings.json')
 settings = json.load(file)
-BOT_TOKEN=settings['TOKEN']
+BOT_TOKEN = settings['TOKEN']
 updateDestinataryList()
 
 # After every 5 to 10mins in between run work()
